63e42019ac2462128512f4a51df5715106d477e4
Leandro Macedo <leandro.smacedo@Live.com>
2024-03-13 14:22:28 +0000

    feat: typography

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import { Button } from "@/components/button";
 import { Input } from "@/components/input";
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
