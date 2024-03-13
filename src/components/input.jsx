81f3d836dd65ba51eb08e8cb0da0b639dbbc0f64
Leandro Macedo <leandro.smacedo@Live.com>
2024-03-13 14:42:52 +0000

    feat: change input

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -7,8 +7,7 @@ export function Input({ label, type, id, onChange, required, placeholder }) {
     return (
         <div className="flex gap-2 flex-col">
             <label for="fname">{label}</label>
-            <input
-                className="border-2 border-zinc-950 px-4 py-2 rounded-2xl">
+            <input className="border-2 text-base border-zinc-950 px-4 py-2 rounded-2xl capitalize">
             </input>
         </div>
     )
