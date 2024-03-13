64a913cba5a90f130cfb9f3a3c982616c6be6224
Carolina Limeres <carolinalimeres@outlook.com>
2024-03-13 22:04:03 +0000

    login page

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -5,10 +5,15 @@ export function Button({ children, variant }) {
     <>
       <button
         variant={variant}
-        className={` font-bold border-2 border-zinc-950  rounded-2xl 
+        className={` flex w-full content-center justify-center items-center gap-2 font-bold border-2 border-zinc-950  rounded-2xl 
         ${variant == "outline" ? " bg-white" : " bg-zinc-950 text-white"} ${
           variant == "button" ? " bg-zinc-200 px-2 py-2" : ""
-        }px-4 py-2 capitalize text-base`}
+        } ${
+          variant == "loginButton"
+            ? " bg-slate-300 text-zinc-950 border-0 w-full"
+            : ""
+        }
+        px-4 py-2 capitalize text-base`}
       >
         {children}
       </button>
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,59 @@
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+import Link from "next/link";
+import { Apple, Google } from "iconsax-react";
+
+export default function Login() {
+  return (
+    <div className=" p-4 ">
+      <div>
+        <h1 className=" text-3xl">Log in</h1>
+      </div>
+      <div className=" h-screen flex justify-center flex-col gap-2">
+        <div>
+          <Input label="Email:" />
+        </div>
+        <div className=" mt-4">
+          <Input label="Senha:" />
+        </div>
+        <div className=" justify-center flex gap-1 mt-4">
+          <p className=" text-xs text-zinc-400">Esqueceu a senha?</p>
+          <Link href="/" className=" text-xs text-blue-500">
+            Clique aqui
+          </Link>
+        </div>
+        <div className=" mt-6 flex justify-center">
+          <Button>Log in</Button>
+        </div>
+        <div
+          className="mt-8 flex gap-4 w-full  justify-center items-center content-center
+"
+        >
+          <div className="  w-full h-px bg-slate-200"></div>
+          <p className="w-full text-xs flex justify-center">ou continue com</p>
+          <div className="w-full h-px bg-slate-200"></div>
+        </div>
+        <div className=" mt-2 flex justify-center gap-2">
+          <div className="w-full flex">
+            <Button variant="loginButton">
+              <Google size="32" color="#000" variant="Bold" />
+              Google
+            </Button>
+          </div>
+          <div className="w-full">
+            <Button variant="loginButton">
+              <Apple size="32" color="#000" variant="Bold" />
+              Apple
+            </Button>
+          </div>
+        </div>
+        <div className=" justify-center flex gap-1 mt-6">
+          <p className=" text-xs text-zinc-400">Ainda não tem uma conta?</p>
+          <Link href="/" className=" text-xs text-blue-500">
+            Clique aqui
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
