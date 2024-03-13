901a39cdb33bbbb9a9617195a36ef107564fe2c7
Leandro Macedo <leandro.smacedo@Live.com>
2024-03-13 16:17:28 +0000

    feat: navbar
    Co-Authored-By: Carolina Limeres <111010122+CarolLimeres@users.noreply.github.com>

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,49 @@
-export function Navbar({ children }) {
+
+import { User, SearchNormal, GlobalSearch, People, Calendar1 } from "iconsax-react";
+
+import Link from "next/link";
+
+export function Navbar({ isHome, isProfile, isReservations }) {
   return (
-    <>
-      <div className="md:max-w-96 mx-auto fixed bottom-0 flex w-full h-12 bg-red px-5">
-        {children}
+    <div className="p-4">
+      <div className=" p-2 text-center fixed bottom-0 left-0 right-0 border-t-2 ">
+        <div className="h-full  flex gap-2 items-center justify-evenly align-center">
+
+
+          <Link href={`/search`}>
+            <div className="flex items-center flex-col justify-center cursor-pointer">
+              <SearchNormal
+                className={`${isProfile ? "text-blue-400" : "text-white-400"
+                  }`}
+                size="24"
+                variant={isProfile ? "Bulk" : "Outline"}
+              />
+            </div>
+          </Link>
+
+          <Link href={`/reservations`}>
+            <div className="flex items-center flex-col justify-center cursor-pointer">
+              <Calendar1
+                className={`${isReservations ? "text-blue-400" : "text-white-400"
+                  }`}
+                size="24"
+                variant={isReservations ? "Bulk" : "Outline"}
+              />
+            </div>
+          </Link>
+
+          <Link href={`/profile/`}>
+            <div className="flex items-center flex-col justify-center cursor-pointer">
+              <User
+                className={`${isHome ? "text-blue-400" : "text-white-400"
+                  }`}
+                size="24"
+                variant={isHome ? "Bulk" : "Outline"}
+              />
+            </div>
+          </Link>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/pages/playgrond/index.jsx
+++ src/pages/playgrond/index.jsx
@@ -1,11 +1,13 @@
 import { Card } from "@/components/card";
 import { Input } from "@/components/input";
+import { Navbar } from "@/components/navbar";
 import { TopNav } from "@/components/topNav";
 
 export default function Playground() {
     return (
         <div>
-            <TopNav>Profile</TopNav>
+            {/* <TopNav>Profile</TopNav> */}
+            <Navbar />
             {/* <Card>
                 <h1 className="text-2xl">Card Title</h1>
                 <p className="font-bold">dsdsad</p>
