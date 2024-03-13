3909e346ed11c6c3ff9690a298d489ec1f8fd0f1
Leandro Macedo <leandro.smacedo@Live.com>
2024-03-13 16:43:30 +0000

    typography

--- src/pages/playgrond/index.jsx
+++ src/pages/playgrond/index.jsx
@@ -2,11 +2,15 @@ import { Card } from "@/components/card";
 import { Input } from "@/components/input";
 import { Navbar } from "@/components/navbar";
 import { TopNav } from "@/components/topNav";
+import { Description, Subtitle, Title } from "@/components/typography/title";
 
 export default function Playground() {
     return (
         <div>
             {/* <TopNav>Profile</TopNav> */}
+            <Title>Happy</Title>
+            <Subtitle>Happy</Subtitle>
+            <Description>Happy</Description>
             <Navbar />
             {/* <Card>
                 <h1 className="text-2xl">Card Title</h1>
