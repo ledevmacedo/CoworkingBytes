81fbdba3062f86227096864f0e795f5cd12717b0
Fabio Silva <121696024+FabioSilva-code@users.noreply.github.com>
2024-03-14 00:20:34 +0000

    disponiveis

--- src/data/espacos/allSpaces.js
+++ src/data/espacos/allSpaces.js
@@ -5,13 +5,15 @@ const collectionName = "Reservas"
 
 // irá ser usado para obter o que n pode ser mostrado
 async function encontrarSalasOcupadas(horaInicio, horaFim, dia) {
+    console.log("valores salas Ocupadas (horaFim,horaInicio,dia)" + typeof (horaFim), horaInicio, dia) // aqui tenho valores
     const collection = await getMongoCollection(collectionName)
     const agregacao = await collection.aggregate([
         {
             $match: {
-                diaReservado: dia,
-                horaInicio: { $lt: horaFim },
-                horaFim: { $gt: horaInicio }
+                dia: Number(dia),
+                horaInicio: { $lt: Number(horaFim) },
+                horaFim: { $gt: Number(horaInicio) }
+
             }
         },
         {
@@ -35,15 +37,14 @@ async function encontrarSalasOcupadas(horaInicio, horaFim, dia) {
                 // Caso contrário, inclui (inclui todos que não são "Coletiva" independentemente da soma de lugares)
                 $or: [
                     { isColetiva: false },
-                    { isColetiva: true, totalLugaresColetiva: { $lt: 200 } }
+                    { isColetiva: true, totalLugaresColetiva: { $gt: 200 } }
                 ]
             }
         }
 
     ]).toArray()
-    console.log(agregacao)
     const idsEspacosOcupados = agregacao.map(item => item._id)
-    console.log(idsEspacosOcupados) //array vazio SEMPRE !?!?!
+    console.log("Agregacao"+ idsEspacosOcupados)
     return idsEspacosOcupados
 }
 
