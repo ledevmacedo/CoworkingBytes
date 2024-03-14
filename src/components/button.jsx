3d856b4bfa6b11ecb5a461141aa02835dbaee983
Leandro Macedo <leandro.smacedo@Live.com>
2024-03-14 01:48:27 +0000

    fix ternary

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -6,14 +6,12 @@ export function Button({ children, variant, onClick }) {
       <button
         onClick={onClick}
         variant={variant}
-        className={` flex w-full content-center justify-center items-center gap-2 font-bold border-2 border-zinc-950  rounded-2xl 
-        ${variant == "outline" ? " bg-white" : " bg-zinc-950 text-white"} ${
-          variant == "button" ? " bg-zinc-200 px-2 py-2" : ""
-        } ${
-          variant == "loginButton"
-            ? " bg-slate-300 text-zinc-950 border-0 w-full"
-            : ""
-        }
+        className={` 
+        flex w-full content-center justify-center items-center gap-2 font-bold rounded-2xl
+        ${variant == "loginButton" ? " bg-slate-300 text-zinc-950  w-full" : ""} 
+        ${variant == "outline" ? " bg-white" : ""} 
+        ${variant == "button" ? " bg-zinc-200 px-2 py-2" : ""}
+        ${!variant ? "bg-zinc-950 text-white" : ""}
         px-4 py-2 capitalize text-base`}
       >
         {children}
